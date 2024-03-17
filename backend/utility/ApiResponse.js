d16bf467a323d6eba31e3577d2b17300b2c8c31b
raJiB <pokhrelrajib016@gmail.com>
2024-03-17 20:32:59 +0545

    added some utilities

---
+++ backend/utility/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse {
+    constructor(statusCode, data, messsage = "success") {
+        this.statusCode = statusCode,
+            this.data = data,
+            this.messsage = messsage
+        this.success = statusCode < 400
+    }
+}
+
+export default ApiResponse
