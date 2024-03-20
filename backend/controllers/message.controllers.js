c64d7c8bee6050d995b2ef098c9a1f2d6001355d
raJiB <pokhrelrajib016@gmail.com>
2024-03-20 20:14:54 +0545

    get messages controllers and routes

--- backend/controllers/message.controllers.js
+++ backend/controllers/message.controllers.js
@@ -1,6 +1,7 @@
 import { ApiError, ApiResponse, asyncHandler } from "../utility/index.js"
-import {Conversation} from "../model/conversation.model.js"
-import {Message} from "../model/message.model.js"
+import { Conversation } from "../model/conversation.model.js"
+import { Message } from "../model/message.model.js"
+import mongoose, { isValidObjectId } from "mongoose"
 
 const sendMessage = asyncHandler(async (req, res) => {
     const { message } = req.body;
@@ -23,10 +24,29 @@ const sendMessage = asyncHandler(async (req, res) => {
         throw new ApiError(500, "Cannot create new message. Try again")
     }
     const success = conversation.messages.push(newMessage._id)
+
+    //socket io
     conversation.save()
     return res.status(200).json(new ApiResponse(200, newMessage, "message sent successfully"))
 
 })
 
+const getMessages = asyncHandler(async (req, res) => {
+    const { id: userToChatId } = req.params
+    const senderId = req.user?._id;
+    if (!((isValidObjectId(userToChatId)) || senderId)) {
+        throw new ApiError(400, "invalid user id or reciver id")
+    }
 
-export { sendMessage }
+    const conversation = await Conversation.findOne({
+        participants: { $all: [senderId, userToChatId] }
+    }).populate("messages");
+    const messages = conversation.messages;
+    if (!conversation) {
+        return res.status(200).json(new ApiResponse(200, [], "no conversation with the user"))
+    }
+    return res.status(200).json(new ApiResponse(200, messages, "messages fetched successfully"))
+
+})
+
+export { sendMessage, getMessages }
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,10 +1,11 @@
 import express, { Router } from "express"
-import { sendMessage } from "../controllers/message.controllers.js"
+import { sendMessage, getMessages } from "../controllers/message.controllers.js"
 import verifyUser from "../middlewares/verifyUser.js"
 
 const messageRouter = new Router()
 
 messageRouter.post("/send/:id", verifyUser, sendMessage)
+messageRouter.get("/:id", getMessages)
 
 
 export default messageRouter
