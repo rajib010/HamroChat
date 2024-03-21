576bbaa39c408e0637af7ac502bb22b785f03b58
raJiB <pokhrelrajib016@gmail.com>
2024-03-21 20:06:27 +0545

    added home page layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
-export default function App() {
+import React from 'react'
+import Login from './pages/login/Login.jsx'
+
+const App = () => {
   return (
-    <>
-      <button className="btn btn-square btn-outline">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-      </button>
-    </>
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <Login />
+    </div>
   )
 }
+
+export default App
