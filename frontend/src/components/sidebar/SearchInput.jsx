a81614117d8299cf0148fd7177b07ec733d0978f
raJiB <pokhrelrajib016@gmail.com>
2024-03-25 15:06:30 +0545

    created and assembled the components of message container

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -6,10 +6,10 @@ import { FaSearch } from "react-icons/fa";
 function SearchInput() {
   return (
     <form className='flex items-center gap-2'>
-        <input type="text" placeholder="Search.." className="input input-bordered w-full max-w-xs" />
-        <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
-            <FaSearch  className='w-6 h-6 outline-none' />
-        </button>
+      <input type="text" placeholder="Search.." className="input input-bordered w-full max-w-xs" />
+      <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
+        <FaSearch className='w-6 h-6 outline-none' />
+      </button>
     </form>
   )
 }
