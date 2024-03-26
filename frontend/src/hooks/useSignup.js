892a75834efb86c0ec78adfa0f189516beabd9f1
raJiB <pokhrelrajib016@gmail.com>
2024-03-26 15:53:41 +0545

    added logout functionality

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,60 +1,57 @@
-import { set } from "mongoose"
-import { useState } from "react"
-import toast from "react-hot-toast"
-import { useAuthContext } from "../context/AuthContext.jsx"
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext.jsx";
 
 const useSignup = () => {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   // from context
-  const{setAuthUser} = useAuthContext();
+  const { setAuthUser } = useAuthContext();
 
   const signup = async ({ fullName, userName, email, password, confirmPassword, gender }) => {
-    const success = handleInputErrors({ fullName, email, userName, password, confirmPassword, gender })
+    const success = handleInputErrors({ fullName, email, userName, password, confirmPassword, gender });
     if (!success) return;
     setLoading(true);
-
     try {
-      const res = await ("/api/auth/signup", {
+      const res = await fetch("/api/auth/signup", { 
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ fullName, userName, email, password, confirmPassword, gender })
       });
 
       const data = await res.json();
-      if(data.error){
-        throw new Error(data.error)
+      if (data.error) {
+        throw new Error(data.error);
       }
-      localStorage.setItem("chat-user", JSON.stringify(data))
+      localStorage.setItem("chat-user", JSON.stringify(data));
 
       //context
-      setAuthUser(data);
+      setAuthUser(data); 
       console.log(data);
     } catch (error) {
-      toast.error(error.message)
+      toast.error(error.message);
     } finally {
       setLoading(false);
     }
-  }
+  };
 
-  return {loading, signup}
-}
+  return { loading, signup };
+};
 
-export default useSignup
-
+export default useSignup;
 
 function handleInputErrors({ fullName, userName, email, password, confirmPassword, gender }) {
   if (!fullName || !userName || !email || !password || !confirmPassword || !gender) {
-    toast.error("Please fill in all the fields")
-    return false
+    toast.error("Please fill in all the fields");
+    return false;
   }
   if (password !== confirmPassword) {
-    toast.error("password donot match")
-    return false
+    toast.error("Password does not match");
+    return false;
   }
   if (password.length < 6) {
-    toast.error("Password mustnot be less than 6 characters")
-    return false
+    toast.error("Password must not be less than 6 characters");
+    return false;
   }
 
   return true;
